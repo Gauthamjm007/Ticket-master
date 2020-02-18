e526c9f6b028471bd27971e5f643eee196cbc2ee
Gauthamjm007 <j.mgautham15@gmail.com>
2020-02-18 12:56:04 +0530

    Updates

---
+++ precache-manifest.cac0c4dd595be1c481edab93007840f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc01b2c03309e57b70c420ada0ad936",
+    "url": "/Ticket-master/index.html"
+  },
+  {
+    "revision": "3240cc02b3b8b07c61b5",
+    "url": "/Ticket-master/static/js/2.66e85b7c.chunk.js"
+  },
+  {
+    "revision": "c8bc72aeae2aa022d3c31673bf728550",
+    "url": "/Ticket-master/static/js/2.66e85b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5399e23b4ed0e4315baf",
+    "url": "/Ticket-master/static/js/main.bbf79a95.chunk.js"
+  },
+  {
+    "revision": "0491e0282dc21573969f",
+    "url": "/Ticket-master/static/js/runtime-main.41415f80.js"
+  }
+]);
